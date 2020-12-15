ab38fe024dda44005f81b0d9e73eeba68fe197cd
jacksoncodispoti <jackson.codispoti@uky.edu>
2020-12-14 20:03:42 -0500

    Added configuration files and updated dataset

---
+++ config.py
@@ -0,0 +1,83 @@
+import ConfigParser
+from collections import namedtuple
+
+conf_path = 'config.config'
+
+conf_disagg = 'disaggregator'
+conf_dataset = 'dataset'
+
+def get_disagg_settings(house_num):
+    master_parser = ConfigParser.RawConfigParser()
+    master_parser.read(conf_path)
+
+    indiv_parser = ConfigParser.RawConfigParser()
+    indiv_parser.read('dataset/house_{}/{}'.format(house_num, conf_path))
+
+    master_settings = { key : master_parser.get(conf_disagg, key) for key in master_parser.options(conf_disagg) }
+
+    if conf_disagg in indiv_parser.sections():
+        indiv_settings = { key : indiv_parser.get(conf_disagg, key) for key in indiv_parser.options(conf_disagg) }
+    else:
+        indiv_settings = {}
+
+    #Overwrite master dict with indiv settings
+    master_settings.update(indiv_settings)
+
+    print(master_settings)
+    return DisaggSettings(**master_settings)
+
+def get_dataset_settings(house_num):
+    master_parser = ConfigParser.RawConfigParser()
+    master_parser.read(conf_path)
+
+    indiv_parser = ConfigParser.RawConfigParser()
+    indiv_parser.read('house_{}/{}'.format(house_num, conf_path))
+
+    master_settings = { key : master_parser.get(conf_dataset, key) for key in master_parser.options(conf_dataset) }
+
+    if conf_dataset in indiv_parser.sections():
+        indiv_settings = { key : indiv_parser.get(conf_dataset, key) for key in indiv_parser.options(conf_dataset) }
+    else:
+        indiv_settings = {}
+
+    #Overwrite master dict with indiv settings
+    master_settings.update(indiv_settings)
+    print(master_settings)
+
+    return DatasetSettings(**master_settings)
+
+class DisaggSettings:
+    def __init__(self, start_time, end_time, sigma, ri, t_positive, t_negative, alpha, beta, instancelimit, init_size, frame_size):
+        self.start_time = start_time
+        self.end_time = end_time
+        self.sigma = float(sigma)
+        self.ri = float(ri)
+        self.T_Positive = float(t_positive)
+        self.T_Negative = float(t_negative)
+        self.alpha = float(alpha)
+        self.beta = float(beta)
+        self.instancelimit = int(instancelimit)
+        self.init_size = int(init_size)
+        self.frame_size = int(frame_size)
+
+class DatasetSettings:
+    def __init__(self, start_time, end_time, channel_mask, sort_order):
+        self.start_time = start_time
+        self.end_time = end_time
+
+        if isinstance(channel_mask, list):
+            self.channel_mask = channel_mask
+        else:
+            if not channel_mask:
+                self.channel_mask = []
+            else:
+                self.channel_mask = [s.strip() for s in channel_mask.split(',')]
+
+        if isinstance(sort_order, list):
+            self.sort_order = sort_order
+        else:
+            if not sort_order:
+                self.sort_order = []
+            else:
+                self.sort_order = [s.strip() for s in sort_order.split(',')]
+
--- dataset/create.py
+++ dataset/create.py
@@ -3,16 +3,16 @@ import pandas as pd
 import sys
 import os
 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from config import get_dataset_settings, DatasetSettings
+
 if len(sys.argv) < 2:
     print('Usage: python create.py [house_num]')
     exit(1)
 
-start_time = '2011-04-23'
-end_time = '2011-04-30'
-channel_mask = ['refrigerator', 'kitchen_outlets', 'microwave', 'lighting']
-sort_order = ['refrigerator',  'kitchen_outlets', 'kitchen_outlets2', 'microwave', 'lighting']
-
-house = 'house_' + str(sys.argv[1])
+house_num = sys.argv[1]
+settings = get_dataset_settings(house_num)
+house = 'house_' + str(house_num)
 house_dir = '../../dataset/low_freq/{}/'.format(house)
 
 try:
@@ -27,13 +27,14 @@ disaggr_name = '{}/output_disaggr.csv' .format(house)
 
 channels = []
 channel_names = {}
+print(settings.channel_mask)
 with open(house_dir + 'labels.dat') as label_file:
     csv_reader = csv.reader(label_file, delimiter=' ')
 
     for row in csv_reader:
         num, name = row
 
-        if len(channel_mask) > 0 and not (name in channel_mask):
+        if len(settings.channel_mask) > 0 and not (name in settings.channel_mask):
             print('{} is not in channel_mask'.format(name))
             continue
 
@@ -50,7 +51,7 @@ with open(house_dir + 'labels.dat') as label_file:
             channels.append([house_dir + 'channel_' + num + '.dat', canonical_name])
 
 
-channels.sort(key=lambda x:sort_order.index(x[1]) if x[1] in sort_order else 0)
+channels.sort(key=lambda x:settings.sort_order.index(x[1]) if x[1] in settings.sort_order else 0)
 main1_path = house_dir + 'channel_1.dat'
 main2_path = house_dir + 'channel_2.dat'
 
@@ -67,7 +68,7 @@ disagg = pd.DataFrame(None, main1.index)
 #pd.set_option('display.max_colwidth', -1)
 
 for c_path, c_name in channels:
-    if not (c_name in sort_order):
+    if settings.sort_order and not (c_name in settings.sort_order):
         continue
     print('Processing channel {}'.format(c_name))
     channel_frame = pd.read_csv(c_path, index_col=0, delimiter=' ', names=['Time', c_name])
@@ -78,7 +79,22 @@ for c_path, c_name in channels:
 
 #disagg = disagg.resample('1T').min().round()
 disagg = disagg.resample('1T').max().round()
-disagg[start_time : end_time].to_csv(disaggr_name, columns=sort_order)
+
+if settings.sort_order:
+    columns = settings.sort_order
+else:
+    columns = [c_name for c_path, c_name in channels]
+
+print(disagg.shape)
+if settings.start_time:
+    disagg[settings.start_time : settings.end_time].to_csv(disaggr_name, columns=columns)
+else:
+    disagg.to_csv(disaggr_name, columns=columns)
+
 main_agg = disagg.transpose()
 main_agg = main_agg.sum()
-main_agg[start_time : end_time].to_csv(aggr_name, index_label='Time', header=['aggregate'])
+
+if settings.start_time:
+    main_agg[settings.start_time : settings.end_time].to_csv(aggr_name, index_label='Time', header=['aggregate'])
+else:
+    main_agg.to_csv(aggr_name, index_label='Time', header=['aggregate'])
